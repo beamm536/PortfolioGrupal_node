55a3aa4b3d9f6c195e4e2381acbc38180cfe056f
beamm536 <beamm023@gmail.com>
2024-11-24 14:20:27 +0100

    archivo rutas
    - he pasado la conexion y las rutas a cada archivo del programa a rutas.js
    - en index.js están otras configuraciones

--- index.js
+++ index.js
@@ -8,67 +8,26 @@ nodemon (para iniciar el proyecto)
 //IMPORTACIONES
 const express = require("express");
 const exphbs = require("express-handlebars");   
-const mysql = require("mysql2"); //importacion para la base de datos
+const rutas = require("./routes/rutas");//para importar las rutas desde el archivo rutas.js
 
 //servidor aplicaciones
 const app = express();
 
-//PUERTOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+//---------------------------PUERTOOOOOOOOOOOOOOO----------------------
 const port = 3000;
 
 //config handlebars
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
-//crear la conexion
-const conexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'programacion2023', 
-    database: 'portfolio'
-});
+//ruta para acceder a recursos estaticos --> lo q nos cargaria de la carpeta publica de plantilla q hemos incorporado
+app.use(express.static("public"));
 
-conexion.connect((err) => {
-    if (err) throw err;
-    console.log('Conexión a la base de datos establecida');
-  });
-
-
-
-// Ruta para obtener los acontecimientos
-app.get('/api/acontecimientos', (req, res) => {
-    const query = 'SELECT p.nombre_participante, p.descripcion_participante, a.nombre_acontecimiento, a.descripcion_acontecimiento, a.img_acontecimiento, a.pers_acontecimiento FROM participantes p LEFT JOIN acontecimientos a ON p.nombre_participante = a.pers_acontecimiento;';
-    
-    conexion.query(query, (err, results) => {
-      if (err) {
-        res.status(500).send('Error en la base de datos');
-        return;
-      }
-  
-      console.log('Resultados de los acontecimientos:', results);  // Imprimir los resultados de la consulta
-      res.json(results);  // Enviar los resultados como JSON
-    });
-  });
-
-
-app.use(express.static("public"));//ruta para acceder a recursos estaticos --> lo q nos cargaria de la carpeta publica de plantilla q hemos incorporado
-
-app.get('/portfIndividual1', (req, res) => {
-    res.render('portfIndividual1', { 
-        titulo: 'Página portfolio individual', 
-        layout: false //desactivar q busque el archivo en la carpeta layout dentro de views 
-    });
-});
-app.get('/', (req, res) => {
-    res.render('index', { 
-        titulo: 'Página Principal_portfolio', 
-        layout: false //desactivar q busque el archivo en la carpeta layout dentro de views 
-    });
-});
+//para usar el archivo de las rutas
+app.use(rutas);
 
 
 //iniciamos el servidor en el puerto 3000
-//app.listen(3000);
 app.listen(3000, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
