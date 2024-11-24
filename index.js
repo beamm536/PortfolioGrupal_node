04f7c4e1200448995df29d6eb7897ebfe4975efd
beamm536 <beamm023@gmail.com>
2024-11-24 13:27:30 +0100

    unión portfolio individual Alba
    - incorporada la libreria de Cors
    - pasado el archivo a .handlebars

--- index.js
+++ index.js
@@ -25,14 +25,44 @@ const conexion = mysql.createConnection({
     database: 'portfolio'
 });
 
+conexion.connect((err) => {
+    if (err) throw err;
+    console.log('Conexión a la base de datos establecida');
+  });
+
+
+
+// Ruta para obtener los acontecimientos
+app.get('/api/acontecimientos', (req, res) => {
+    const query = 'SELECT p.nombre_participante, p.descripcion_participante, a.nombre_acontecimiento, a.descripcion_acontecimiento, a.img_acontecimiento, a.pers_acontecimiento FROM participantes p LEFT JOIN acontecimientos a ON p.nombre_participante = a.pers_acontecimiento;';
+    
+    conexion.query(query, (err, results) => {
+      if (err) {
+        res.status(500).send('Error en la base de datos');
+        return;
+      }
+  
+      console.log('Resultados de los acontecimientos:', results);  // Imprimir los resultados de la consulta
+      res.json(results);  // Enviar los resultados como JSON
+    });
+  });
+
+
 app.use(express.static("public"));//ruta para acceder a recursos estaticos --> lo q nos cargaria de la carpeta publica de plantilla q hemos incorporado
 
+app.get('/portfIndividual1', (req, res) => {
+    res.render('portfIndividual1', { 
+        titulo: 'Página portfolio individual', 
+        layout: false //desactivar q busque el archivo en la carpeta layout dentro de views 
+    });
+});
 app.get('/', (req, res) => {
     res.render('index', { 
         titulo: 'Página Principal_portfolio', 
         layout: false //desactivar q busque el archivo en la carpeta layout dentro de views 
     });
 });
+
 
 //iniciamos el servidor en el puerto 3000
 //app.listen(3000);
