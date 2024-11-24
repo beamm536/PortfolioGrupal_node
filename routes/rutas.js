41801d2fc554913e42e9b70fed839bec37167207
beamm536 <beamm023@gmail.com>
2024-11-24 14:20:27 +0100

    archivo rutas
    - he pasado la conexion y las rutas a cada archivo del programa a rutas.js
    - en index.js están otras configuraciones

--- routes/rutas.js
+++ routes/rutas.js
@@ -1,12 +1,63 @@
 const express = require("express")
+const router = express.Router();
+const mysql = require("mysql2"); //importacion para la base de datos
+
+
+//crear la conexion
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'programacion2023', 
+    database: 'portfolio'
+});
+
+conexion.connect((err) => {
+    if (err) throw err;
+    console.log('Conexión a la base de datos establecida');
+  });
+
+
+// Ruta para obtener los acontecimientos
+router.get('/api/acontecimientos', (req, res) => {
+    const query = 'SELECT p.nombre_participante, p.descripcion_participante, a.nombre_acontecimiento, a.descripcion_acontecimiento, a.img_acontecimiento, a.pers_acontecimiento FROM participantes p LEFT JOIN acontecimientos a ON p.nombre_participante = a.pers_acontecimiento;';
+    
+    conexion.query(query, (err, results) => {
+      if (err) {
+        res.status(500).send('Error en la base de datos');
+        return;
+      }
+  
+      console.log('Resultados de los acontecimientos:', results);  // Imprimir los resultados de la consulta
+      res.json(results);  // Enviar los resultados como JSON
+    });
+  });
+
+//RUTA PARA LA PAGINA DE PRUEBA PORTFOLIO INDIVIDUAL
+router.get('/portfIndividual1', (req, res) => {
+    res.render('portfIndividual1', { 
+        titulo: 'Página portfolio individual', 
+        layout: false //desactivar q busque el archivo en la carpeta layout dentro de views 
+    });
+});
+//RUTA PARA EL INDEX
+router.get('/', (req, res) => {
+    res.render('index', { 
+        titulo: 'Página Principal_portfolio', 
+        layout: false //desactivar q busque el archivo en la carpeta layout dentro de views 
+    });
+});
+
+
+
+//para traerme el enrutado desde el index --> EXPORTAMOS EL ENRUTADOR DE LA APP
+module.exports = router;
+
+
 
 //traemos el enrutador def express
+/*
+ESTE ES EL CODIGO DE RUBEN Q TENIA EN ESTE ARCHIVO
 const router = express.Router();
 router.get("/", (req, res)=>{
     res.render("misitioHome", {layout:"misitio"});
-});
-
-//para traerme el enrutado desde el index
-module.exports = router;
-
-//como lo integramos --> desde index.js
+});*/
